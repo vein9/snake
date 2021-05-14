0f915b9df5541d458e818d756640b1bdc21ccb64
thaison111299 <thaison111299@gmail.com>
2021-05-15 03:57:58 +0700

    Unity style

---
+++ index.js
@@ -0,0 +1,148 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+canvas.width = canvas.height = 600
+ctx.fillStyle = 'black'
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+const UNIT = 20
+const LEFT = 37
+const UP = 38
+const RIGHT = 39
+const DOWN = 40
+
+class Vector2d {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+class Snake {
+    constructor() {
+        this.head = new Vector2d(200, 100)
+        this.body = [
+            new Vector2d(180, 100),
+            new Vector2d(160, 100),
+        ]
+        this.velocity = new Vector2d(1, 0)
+    }
+    draw() {
+        ctx.fillStyle = 'blue'
+        ctx.fillRect(this.head.x, this.head.y, UNIT, UNIT)
+
+
+        ctx.fillStyle = 'white'
+        for (let cube of this.body) {
+            ctx.fillRect(cube.x, cube.y, UNIT, UNIT)
+        }
+    }
+
+    clear() {
+        ctx.fillStyle = 'black'
+        ctx.fillRect(this.head.x, this.head.y, UNIT, UNIT)
+        ctx.fillStyle = 'black'
+        for (let cube of this.body) {
+            ctx.fillRect(cube.x, cube.y, UNIT, UNIT)
+        }
+
+    }
+
+    move() {
+        this.clear()
+        let tmp = new Vector2d(this.head.x, this.head.y)
+
+        this.head.x += this.velocity.x * UNIT
+        this.head.y += this.velocity.y * UNIT
+
+        for (let i = this.body.length - 1; i >= 1; i--) {
+            this.body[i].x = this.body[i - 1].x
+            this.body[i].y = this.body[i - 1].y
+        }
+
+        this.body[0] = tmp
+        this.draw()
+    }
+
+    grow() {
+
+        let mountX = this.body[this.body.length - 1].x - this.body[this.body.length - 2].x
+        let mountY = this.body[this.body.length - 1].y - this.body[this.body.length - 2].y
+
+        let newCube = new Vector2d(this.body[this.body.length - 1].x + mountX, this.body[this.body.length - 1].y - this.body[this.body.length - 1].y + mountY)
+        this.body.push(newCube)
+        this.clear()
+        this.draw()
+    }
+
+    eatFood(food) {
+        if (food.pos.x === this.head.x && food.pos.y === this.head.y) {
+            this.grow()
+            food.spawn()
+        }
+    }
+}
+
+class Food {
+    constructor() {
+        this.pos = new Vector2d(0, 0)
+        this.color = 'green'
+    }
+
+    draw() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.pos.x, this.pos.y, UNIT, UNIT)
+    }
+    clear() {
+        ctx.fillStyle = 'black'
+        ctx.fillRect(this.pos.x, this.pos.y, UNIT, UNIT)
+    }
+    getRandomNumber() {
+        let result = Math.floor(Math.random() * canvas.width)
+        result -= result % 20
+        return result
+    }
+    spawn() {
+        this.clear()
+        this.pos.x = this.getRandomNumber()
+        this.pos.y = this.getRandomNumber()
+        this.draw(  )
+    }
+}
+
+document.onkeydown = getInputKey
+
+
+
+let player = new Snake()
+player.draw()
+
+let food = new Food()
+food.spawn()
+
+
+function getInputKey(e) {
+    switch (e.keyCode) {
+        case UP:
+            player.velocity.x = 0
+            player.velocity.y = -1
+            break;
+        case DOWN:
+            player.velocity.x = 0
+            player.velocity.y = 1
+            break;
+        case LEFT:
+            player.velocity.x = -1
+            player.velocity.y = 0
+            break;
+        case RIGHT:
+            player.velocity.x = 1
+            player.velocity.y = 0
+            break;
+
+    }
+}
+
+
+setInterval(() => {
+    player.move()
+    player.eatFood(food)
+}, 200);
